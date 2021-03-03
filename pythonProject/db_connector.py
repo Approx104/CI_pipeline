e76bb8203423582f77107db22b110b39e55eb949
Approx104 <aranor104@gmail.com>
2021-03-04 00:12:00 +0200

    Test outside connection

--- pythonProject/db_connector.py
+++ pythonProject/db_connector.py
@@ -2,7 +2,7 @@ import pymysql
 
 # Establishing a connection to DB
 try:
-    conn = pymysql.connect(host='185.240.142.179', port=3306, user='root', passwd='Aa123456', db='sys')
+    conn = pymysql.connect(host='3.142.46.13', port=3306, user='username', passwd='password', db='sys')
 except pymysql.err.OperationalError:
     try:
         conn = pymysql.connect(host='host.docker.internal', port=3306, user='root', passwd='Aa123456', db='sys')
